1f35004d0fe28d32bf335a45299c20ba676ef074
= <=>
2021-06-08 17:31:49 +0700

    Đã kết nối DB bằng Database First và đang code tính năng đăng ký

--- WebMain/WebMain/Controllers/UILoginController.cs
+++ WebMain/WebMain/Controllers/UILoginController.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMain.Models;
 
 namespace WebMain.Controllers
 {
     public class UILoginController : Controller
     {
+        private egvietnam_dbEntities db = new egvietnam_dbEntities();
+
         // GET: UILogin
         public ActionResult Index()
         {
             return View();
         }
+
+        //Register
         public ActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(AspNetUser reg)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AspNetUsers.Add(reg);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
--- WebMain/WebMain/Models/AspNetUser.cs
+++ WebMain/WebMain/Models/AspNetUser.cs
@@ -11,35 +11,38 @@ namespace WebMain.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class AspNetUser
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public AspNetUser()
-        {
-            this.AspNetUserClaims = new HashSet<AspNetUserClaim>();
-            this.AspNetUserLogins = new HashSet<AspNetUserLogin>();
-            this.AspNetUserRoles = new HashSet<AspNetUserRole>();
-        }
-    
+        [Key]
         public string Id { get; set; }
+
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
         public bool EmailConfirmed { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
         public string PasswordHash { get; set; }
+
         public string SecurityStamp { get; set; }
+
+        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
+
         public bool PhoneNumberConfirmed { get; set; }
+
         public bool TwoFactorEnabled { get; set; }
+
         public Nullable<System.DateTime> LockoutEndDateUtc { get; set; }
+
         public bool LockoutEnabled { get; set; }
+
         public int AccessFailedCount { get; set; }
+
+        [Display(Name = "Username")]
         public string UserName { get; set; }
-    
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<AspNetUserClaim> AspNetUserClaims { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<AspNetUserLogin> AspNetUserLogins { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<AspNetUserRole> AspNetUserRoles { get; set; }
     }
 }
